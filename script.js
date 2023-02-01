e823dfe53c04c7e59daf71da6d5108745a96c7dc
Isaiah David <isaiahd10@icloud.com>
2023-02-01 01:04:32 -0500

    Add hero

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function myFunction() {
-    var x = document.getElementById("myNavBar");
+    let x = document.getElementById("myNavBar");
     if (x.className === "navBar") {
       x.className += " responsive";
     } else {
       x.className = "navBar";
     }
   }
+
+function nav(){
+        window.location.hash= "#me"
+     }
